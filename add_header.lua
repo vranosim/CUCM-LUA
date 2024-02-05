8d71396653947e6cfd74aa499829e8f4b2a2415d
vranosim <158761843+vranosim@users.noreply.github.com>
2024-02-05 08:18:18 +0100

    Update add_header.lua

--- add_header.lua
+++ add_header.lua
@@ -1,1 +1,24 @@
-
+M = {}
+ 
+trace.enable()
+  
+  function M.inbound_INVITE(msg) 
+      
+    trace.format("LUA-TRACE: INITIALIZE LUA SCRIPT")
+    
+    local headerTo = msg:getHeader("To")
+    trace.format(headerTo)
+    
+    pt = msg:getPassThrough()
+    
+    if headerTo:find("777%d%d%d%d%d") then
+        trace.format("Called number begins with 777")
+        pt:addHeader("X-new-header", "777")
+    else
+        trace.format("called number does not begin with 777")
+        pt:addHeader("X-new-header", "non-777")
+    end
+    
+    end
+   
+return M
